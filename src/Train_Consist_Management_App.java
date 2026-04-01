81cdfa816dd12741b7331ab3403a46f26437c181
sameer_10317 <ijsameersha280605@gmail.com>
2026-04-01 09:32:43 +0530

    UC8: Filter Passenger Bogies Using Streams

--- src/Train_Consist_Management_App.java
+++ src/Train_Consist_Management_App.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import java.util.stream.*;
 
 class Bogie {
     String name;
@@ -18,9 +19,11 @@ public class Train_Consist_Management_App {
         list.add(new Bogie("AC Chair", 54));
         list.add(new Bogie("First Class", 24));
 
-        list.sort(Comparator.comparingInt(b -> b.capacity));
+        List<Bogie> filtered = list.stream()
+                .filter(b -> b.capacity > 60)
+                .collect(Collectors.toList());
 
-        for (Bogie b : list) {
+        for (Bogie b : filtered) {
             System.out.println(b.name + " : " + b.capacity);
         }
     }
